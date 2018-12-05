f7f50632e7bdfc6acc2af94b48e24b24f25596dd
mankydo <45627260+mankydo@users.noreply.github.com>
2018-12-05 00:27:16 -1200

    Add files via upload

---
+++ bracelets.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bracelets</title>
+<style type="text/css">
+#searchForm #search {
+	background-color: #FFF;
+	height: 30px;
+	width: 50px;
+	font-size: 14px;
+	color: #000;
+	border: 1px solid #000;
+	border-radius: 5px;
+}
+
+#searchForm #query {
+	width: 300px;
+	border: 1px solid #999999;
+	border-radius: 5px;
+	height: 20px;
+	font-size: 16px;
+	color: #000000;
+	padding: 5px;
+}
+
+#results {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #666;
+}
+
+#banner {	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #000;
+	font-weight: normal;
+	text-align: right;
+}
+#newshops {	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #000000;
+	background-color: #FFF;
+	border-top-width: 1px;
+	border-right-width: 1px;
+	border-bottom-width: 1px;
+	border-left-width: 1px;
+	border-top-style: solid;
+	border-right-style: solid;
+	border-bottom-style: solid;
+	border-left-style: solid;
+}
+#discount {	border: 1px solid #868583;
+}
+
+a:link {
+	color: #333;
+}
+a:visited {
+	color: #099;
+}
+a:hover {
+	color: #F90;
+}
+</style>
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onload="MM_preloadImages('images/bracelets_over.jpg','images/brooches_over.jpg','images/charms_over.jpg','images/clips_over.jpg','images/earrings_over.jpg','images/necklaces_over.jpg','images/rings_over.jpg','images/bangles_over.jpg','images/shops_over.jpg','images/contact_over.jpg','images/help_over.jpg','images/payment_over.jpg','images/mobile_over.jpg')">
+<table width="624" border="0" id="banner">
+  <tr>
+    <td width="213"><a href="index.php"><img src="images/logo.jpg" width="214" height="80" /></a></td>
+    <td width="401"><form id="searchForm" name="searchForm" method="post" action="search.php">
+      Search for Products
+      <input type="text" name="query" id="query" />
+      <input type="submit" name="search" id="search" value="Go!" />
+    </form>
+      <br />
+      <table id="Table_2" width="640" height="30" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td><a href="floors.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('shops','','images/shops_over.jpg',1)"><img src="images/shops.jpg" alt="shops" name="newshops" width="81" height="30" id="shops" /></a></td>
+          <td><a href="voucherentry.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('payment vouchers','','images/payment_over.jpg',1)"><img src="images/payment.jpg" alt="payment vouchers" width="147" height="30" id="payment vouchers" /></a></td>
+          <td><a href="mobilemoney.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('mobile money','','images/mobile_over.jpg',1)"><img src="images/mobile.jpg" alt="mobile money" width="128" height="30" id="mobile money" /></a></td>
+          <td><a href="vieworders.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('view orders','','images/vieworders_over.jpg',1)"><img src="images/vieworders.jpg" alt="view orders" name="neworders" width="107" height="30" id="view orders" /></a></td>
+          <td><a href="contact.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('contact','','images/contact_over.jpg',1)"><img src="images/contact.jpg" alt="contact" name="contact" width="84" height="30" id="contact" /></a></td>
+          <td><a href="help.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('help','','images/help_over.jpg',1)"><img src="images/help.jpg" alt="help" name="newhelp" width="65" height="30" id="help" /></a></td>
+          <td><img src="images/shops_07.jpg" width="28" height="30" alt="" /></td>
+        </tr>
+      </table>
+      <table id="Table_01" width="640" height="50" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td><a href="bracelets.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('bracelets','','images/bracelets_over.jpg',1)"><img src="images/bracelets.jpg" alt="bracelets" width="98" height="50" id="bracelets" /></a></td>
+          <td><a href="brooches.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('brooches','','images/brooches_over.jpg',1)"><img src="images/brooches.jpg" alt="brooches" width="90" height="50" id="brooches" /></a></td>
+          <td><a href="charms.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('charms','','images/charms_over.jpg',1)"><img src="images/charms.jpg" alt="charms" width="76" height="50" id="charms" /></a></td>
+          <td><a href="clips.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('clips','','images/clips_over.jpg',1)"><img src="images/clips.jpg" alt="clips" width="56" height="50" id="clips" /></a></td>
+          <td><a href="earrings.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('earrings','','images/earrings_over.jpg',1)"><img src="images/earrings.jpg" alt="earrings" width="78" height="50" id="earrings" /></a></td>
+          <td><a href="necklaces.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('necklaces','','images/necklaces_over.jpg',1)"><img src="images/necklaces.jpg" alt="necklaces" width="95" height="50" id="necklaces" /></a></td>
+          <td><a href="rings.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('rings','','images/rings_over.jpg',1)"><img src="images/rings.jpg" alt="rings" width="58" height="50" id="rings" /></a></td>
+          <td><a href="bangles.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('bangles','','images/bangles_over.jpg',1)"><img src="images/bangles.jpg" alt="bangles" width="89" height="50" id="bangles" /></a></td>
+        </tr>
+      </table>
+      <marquee direction='left'>
+        <font color='red'>New Bracelets, Necklaces, Rings in stock at low prices! </font>
+      </marquee>
+      </p></td>
+  </tr>
+</table>
+<table width="200" align="center">
+  <tr>
+    <td><img src="images/bracelets_header.gif" width="200" height="30" /></td>
+  </tr>
+</table>
+<table width="700" border="0" id="results">
+  <tr>
+    <td width="222"><img src="images/selectshop.jpg" width="160" height="600" /></td>
+    <td width="468"><?php
+  
+//connect to server and select database
+include ('connection.php');
+   
+      echo "<table width='300' align='center'>";  
+   
+               
+   
+   $result= mysql_query("SELECT * FROM products WHERE type = 'bracelet'") or die(mysql_error());         
+ if(mysql_num_rows($result) > 0)
+ 
+ {            while($results = mysql_fetch_array($result))   {                
+ echo "<tr><td height='25px'><font size='3px'>".'<a href="description.php?id='.$results['id'].'">'.$results['name']."<tr><td>".$results['type']."</td> 
+  <tr><td><font color='red'><b>Ghc".$results['price']."</td><tr><td>".$results['image']."</td> 
+  <tr><td>".$results['description']."</td><tr><td>".'<a href="floors.php?floor_number='.$results['floor_number'].'">'."Floor Number: ".$results['floor_number']."</td><tr><td>".'<a href="shop.php?shop_number='.$results['shop_number'].'">'."Shop Number: ".$results['shop_number']."</td><tr><td><font color='green'>Contact: ".$results['contact']."</td></tr>" ;          }   
+                  }       
+			
+
+
+
+   
+?></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="700" align="center" id="results">
+  <tr>
+    <td width="217" bgcolor="#FFFFFF">&nbsp;</td>
+    <td colspan="2" bgcolor="#FFFFFF"><a href="index.php"><img src="images/logo.jpg" width="113" height="42" /></a></td>
+    <td width="162" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td><a href="makola.php">Makola Market</a></td>
+    <td width="161"><a href="voucherentry.php">Payment Voucher</a><a href="makola.php"></a><a href="bracelets.php"></a></td>
+    <td width="140"><a href="bracelets.php">Bracelets</a></td>
+    <td><a href="earrings.php">Earrings</a></td>
+  </tr>
+  <tr>
+    <td><a href="contact.php">Contact</a><a href="vieworders.php"></a></td>
+    <td><a href="mobilemoney.php">Mobile Money</a><a href="voucherentry.php"></a><a href="brooches.php"></a></td>
+    <td><a href="brooches.php">Brooches</a></td>
+    <td><a href="necklaces.php">Necklaces</a></td>
+  </tr>
+  <tr>
+    <td><a href="help.php">Help</a><a href="contact.php"></a></td>
+    <td><a href="vieworders.php">View Orders</a><a href="mobilemoney.php"></a><a href="charms.php"></a></td>
+    <td><a href="charms.php">Charms</a></td>
+    <td><a href="rings.php">Rings</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><a href="clips.php">Clips</a></td>
+    <td><a href="bracelets.php">Bangles</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E"><a href="http://www.facebook.com" target="_blank"><img src="images/fb.jpg" width="20" height="20" /></a>Like us <a href="http://www.twitter.com"><img src="images/twitter.jpg" width="20" height="20" /></a>Follow us</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td colspan="2">&copy; 2014 Easytrade.com, or its affiliates</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<style type="text/css">
+#searchForm #query {
+	width: 300px;
+	border: 1px solid #999999;
+	border-radius: 5px;
+	height: 20px;
+	font-size: 16px;
+	color: #000000;
+	padding: 5px;
+}
+#searchForm #search {
+	background-color: #FFF;
+	height: 30px;
+	width: 50px;
+	font-size: 14px;
+	color: #000;
+	border: 1px solid #000;
+	border-radius: 5px;
+}
+#newshops {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #000000;
+	background-color: #FFF;
+	border: 1px solid #000;
+}
+#banner {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #000;
+	font-weight: normal;
+	text-align: right;
+}
+a:link {
+	color: #333;
+}
+a:visited {
+	color: #099;
+}
+a:hover {
+	color: #F90;
+}
+#results {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 12px;
+	color: #666;
+}
+#discount {
+	border: 1px solid #868583;
+}
+</style>
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onload="MM_preloadImages('images/bracelets_over.jpg','images/brooches_over.jpg','images/charms_over.jpg','images/clips_over.jpg','images/earrings_over.jpg','images/necklaces_over.jpg','images/rings_over.jpg','images/bangles_over.jpg','images/images/make_payments_over.jpg','images/images/cupcake_over.jpg','images/horn-bangle_over.jpg','images/shops_over.jpg','images/payment_over.jpg','images/mobile_over.jpg','images/vieworders_over.jpg','images/contact_over.jpg','images/help_over.jpg','images/shopnow_over.jpg','images/check_over.jpg','images/cherries_over.jpg','images/elsa_over.jpg','images/images/soleste_over.jpg','images/images/hv_over.jpg')">
+<table width="624" border="0" id="banner">
+  <tr>
+    <td width="213"><a href="index.php"><img src="images/logo.jpg" width="214" height="80" /></a></td>
+    <td width="401"><form id="searchForm" name="searchForm" method="post" action="search.php">
+      Search for Products  
+      <input type="text" name="query" id="query" />
+      <input type="submit" name="search" id="search" value="Go!" />
+    </form>
+    <br />
+    <table id="Table_2" width="640" height="30" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><a href="floors.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('shops','','images/shops_over.jpg',1)"><img src="images/shops.jpg" alt="shops" name="newshops" width="81" height="30" id="shops" /></a></td>
+        <td><a href="voucherentry.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('payment vouchers','','images/payment_over.jpg',1)"><img src="images/payment.jpg" alt="payment vouchers" width="147" height="30" id="payment vouchers" /></a></td>
+        <td><a href="mobilemoney.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('mobile money','','images/mobile_over.jpg',1)"><img src="images/mobile.jpg" alt="mobile money" width="128" height="30" id="mobile money" /></a></td>
+        <td><a href="vieworders.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('view orders','','images/vieworders_over.jpg',1)"><img src="images/vieworders.jpg" alt="view orders" name="neworders" width="107" height="30" id="view orders" /></a></td>
+        <td><a href="contact.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('contact','','images/contact_over.jpg',1)"><img src="images/contact.jpg" alt="contact" name="contact" width="84" height="30" id="contact" /></a></td>
+        <td><a href="help.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('help','','images/help_over.jpg',1)"><img src="images/help.jpg" alt="help" name="newhelp" width="65" height="30" id="help" /></a></td>
+        <td><img src="images/shops_07.jpg" width="28" height="30" alt="" /></td>
+      </tr>
+    </table>
+    <table id="Table_01" width="640" height="50" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><a href="bracelets.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('bracelets','','images/bracelets_over.jpg',1)"><img src="images/bracelets.jpg" alt="bracelets" width="98" height="50" id="bracelets" /></a></td>
+        <td><a href="brooches.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('brooches','','images/brooches_over.jpg',1)"><img src="images/brooches.jpg" alt="brooches" width="90" height="50" id="brooches" /></a></td>
+        <td><a href="charms.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('charms','','images/charms_over.jpg',1)"><img src="images/charms.jpg" alt="charms" width="76" height="50" id="charms" /></a></td>
+        <td><a href="clips.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('clips','','images/clips_over.jpg',1)"><img src="images/clips.jpg" alt="clips" width="56" height="50" id="clips" /></a></td>
+        <td><a href="earrings.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('earrings','','images/earrings_over.jpg',1)"><img src="images/earrings.jpg" alt="earrings" width="78" height="50" id="earrings" /></a></td>
+        <td><a href="necklaces.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('necklaces','','images/necklaces_over.jpg',1)"><img src="images/necklaces.jpg" alt="necklaces" width="95" height="50" id="necklaces" /></a></td>
+        <td><a href="rings.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('rings','','images/rings_over.jpg',1)"><img src="images/rings.jpg" alt="rings" width="58" height="50" id="rings" /></a></td>
+        <td><a href="bangles.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('bangles','','images/bangles_over.jpg',1)"><img src="images/bangles.jpg" alt="bangles" width="89" height="50" id="bangles" /></a></td>
+      </tr>
+    </table>
+    <marquee direction='left'>
+    <font color='red'>New Bracelets, Necklaces, Rings in stock at low prices! </font>
+    </marquee>
+    </p></td>
+  </tr>
+</table>
+<table width="200" align="right" id="results">
+  <tr>
+    <td><font  color='orange' size='3'>Best Sellers in Earrings</font></td>
+  </tr>
+  <tr>
+    <td><?php
+  
+//connect to server and select database
+include ('connection.php');
+    
+ 
+               
+   
+   $result= mysql_query("SELECT * FROM products WHERE type = 'earring'") or die(mysql_error());         
+ if(mysql_num_rows($result) > 0)
+ 
+ {            while($results = mysql_fetch_array($result))   {                
+ echo "<tr><td>".'<a href="description.php?id='.$results['id'].'">'.$results['image']."</td><tr><td>".'<a href="description.php?id='.$results['id'].'">'.$results['name']."<tr><td>
+  <tr><td><font color='red'>Ghc ".$results['price']."</td></tr>" ;          }   
+                  }       
+			
+
+
+
+   
+?>&nbsp;</td>
+  </tr>
+</table>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+$(function(){
+	$('.fadein img:gt(0)').hide();
+	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+});
+</script>
+ 
+</head>
+<body>
+<table width="701" height="301" border="0" cellpadding="0" cellspacing="0" id="Table_">
+  <tr>
+    <td><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('shop now','','images/shopnow_over.jpg',1)"><img src="images/shopnow.jpg" alt="shop now" width="156" height="102" id="shop now" /></a></td>
+    <td colspan="3"><img src="images/homepic_02.jpg" width="280" height="102" alt="" /></td>
+    <td rowspan="2"><a href="clips.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('cherries red enamel','','images/cherries_over.jpg',1)"><img src="images/cherries.jpg" alt="cherries red enamel" width="264" height="152" id="cherries red enamel" /></a></td>
+    <td><img src="images/spacer.gif" width="1" height="102" alt="" /></td>
+  </tr>
+  <tr>
+    <td colspan="4" rowspan="2"><img src="images/homepic_04.jpg" width="436" height="156" alt="" /></td>
+    <td><img src="images/spacer.gif" width="1" height="50" alt="" /></td>
+  </tr>
+  <tr>
+    <td rowspan="2"><a href="necklaces.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('elsa peretti pendant','','images/elsa_over.jpg',1)"><img src="images/elsa.jpg" alt="elsa peretti pendant" width="264" height="148" id="elsa peretti pendant" /></a></td>
+    <td><img src="images/spacer.gif" width="1" height="106" alt="" /></td>
+  </tr>
+  <tr>
+    <td colspan="2"><img src="images/homepic_06.jpg" width="292" height="42" alt="" /></td>
+    <td><a href="earrings.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('check it','','images/check_over.jpg',1)"><img src="images/check.jpg" alt="check it" width="127" height="42" id="check it" /></a></td>
+    <td><img src="images/homepic_08.jpg" width="17" height="42" alt="" /></td>
+    <td><img src="images/spacer.gif" width="1" height="42" alt="" /></td>
+  </tr>
+  <tr>
+    <td><img src="images/spacer.gif" width="156" height="1" alt="" /></td>
+    <td><img src="images/spacer.gif" width="136" height="1" alt="" /></td>
+    <td><img src="images/spacer.gif" width="127" height="1" alt="" /></td>
+    <td><img src="images/spacer.gif" width="17" height="1" alt="" /></td>
+    <td><img src="images/spacer.gif" width="264" height="1" alt="" /></td>
+    <td></td>
+  </tr>
+</table>
+<table width="700" id="results">
+  <tr>
+    <td width="377"><p><a href="floors.php">Browse the Shops in the Mall to view items they have on sale</a></p>
+    <p>You can find each shop's contact numbers here.</p>
+    <p><a href="contact.php">Contact Us</a></font></a> if you have any questions or having account-related issues, problems with your orders, delivery or other.</p>
+    <p>&nbsp;</p></td>
+    <td width="311"><p>Clicking on Bracelets, Charms, Clips etc. takes you to all corresponding items available in the mall.</p>
+    <p>Make payments for your orders with either an <a href="voucherentry.php">Easytrade Voucher</a> or <a href="mobilemoney.php">mobile money</a>.           </p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+<table width="720" height="25" id="results">
+  <tr>
+    <td width="688">&nbsp;<a href="mobilemoney.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('make payments','','images/images/make_payments_over.jpg',1)"><img src="images/images/make_payments.jpg" alt="make payments" width="215" height="152" id="make payments" /></a><a href="charms.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('cupcake','','images/images/cupcake_over.jpg',1)"><img src="images/images/cupcake.jpg" alt="cupcake" width="218" height="152" id="cupcake" /></a><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('horn bangle','','images/horn-bangle_over.jpg',1)"><img src="images/horn-bangle.jpg" alt="horn bangle" width="264" height="152" id="horn bangle" /></a></td>
+  </tr>
+</table>
+<table width="700" id="results">
+  <tr>
+    <td width="342"><p>Top-up your Balance in your account by buying Easytrade Vouchers at the mall or at any retail shop.</p>
+    <p>&nbsp;</p></td>
+    <td width="346">Registered Customers can view their orders using the <a href="vieworders.php">View Orders</a> link at the top.</td>
+  </tr>
+</table>
+<table width="700" id="results">
+  <tr>
+    <td width="348"><img src="images/images/havevoucher.jpg" width="171" height="150" /><a href="voucherentry.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('hv','','images/images/hv_over.jpg',1)"><img src="images/images/hv.jpg" alt="load" width="177" height="150" id="hv" /></a>      <p>&nbsp;</p></td>
+    <td width="340"><a href="necklaces.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('soleste','','images/images/soleste_over.jpg',1)"><img src="images/images/soleste.jpg" alt="Tiffany Soleste" width="156" height="150" id="soleste" /></a><img src="images/images/soleste1.jpg" width="184" height="150" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="980" id="results">
+  <tr>
+    <td width="245" bgcolor="#FFFFFF">&nbsp;</td>
+    <td colspan="2" bgcolor="#FFFFFF"><a href="index.php"><img src="images/logo.jpg" width="113" height="42" /></a></td>
+    <td width="225" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td><a href="makola.php">Makola Market</a></td>
+    <td width="245"><a href="voucherentry.php">Payment Voucher</a><a href="makola.php"></a><a href="bracelets.php"></a></td>
+    <td width="245"><a href="bracelets.php">Bracelets</a></td>
+    <td><a href="earrings.php">Earrings</a></td>
+  </tr>
+  <tr>
+    <td><a href="contact.php">Contact</a><a href="vieworders.php"></a></td>
+    <td><a href="mobilemoney.php">Mobile Money</a><a href="voucherentry.php"></a><a href="brooches.php"></a></td>
+    <td><a href="brooches.php">Brooches</a></td>
+    <td><a href="necklaces.php">Necklaces</a></td>
+  </tr>
+  <tr>
+    <td><a href="help.php">Help</a><a href="contact.php"></a></td>
+    <td><a href="vieworders.php">View Orders</a><a href="mobilemoney.php"></a><a href="charms.php"></a></td>
+    <td><a href="charms.php">Charms</a></td>
+    <td><a href="rings.php">Rings</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><a href="clips.php">Clips</a></td>
+    <td><a href="bracelets.php">Bangles</a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E">&nbsp;</td>
+    <td bgcolor="#EEB42E"><a href="http://www.facebook.com"><img src="images/fb.jpg" width="20" height="20" /></a>Like us  <a href="http://www.twitter.com"><img src="images/twitter.jpg" width="20" height="20" /></a>Follow us</td>
+  </tr>
+</table>
+<table width="250" align="center" id="results">
+  <tr>
+    <td>&copy;2014 Easytrade.com, or its affiliates</td>
+  </tr>
+</table>
+
+</body>
+</html>
